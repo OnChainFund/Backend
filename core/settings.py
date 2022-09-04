98bcdb90e48263d2ec4cacf26dfeb48279377b58
LJlkdskdjflsa <liliangjya@gmail.com>
2022-09-04 22:01:12 +0800

    feat: renew

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["ocf-backend.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = ["https://ocf-backend.up.railway.app"]
 
