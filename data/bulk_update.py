1da8908e1964c51b62183612644bb61e6ca43b00
LJlkdskdjflsa <liliangjya@gmail.com>
2022-09-19 13:05:51 +0800

    feat: renew

--- data/bulk_update.py
+++
@@ -1,7 +0,0 @@
-from fund.models import Asset, Price, Fund
-
-t2 = Fund.objects.all().filter(name="T2").first()
-print(t2)
-t2_price = Price.objects.all().filter(fund=t2)
-print(t2_price)
-t2_price.update(nav_per_share=15)
