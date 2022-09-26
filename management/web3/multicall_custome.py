9dec817b62fa24d2712d14c5367c4c5aca7f52aa
LJlkdskdjflsa <liliangjya@gmail.com>
2022-09-26 22:56:39 +0800

    feat: renew: fail to use multicall, add django-ethereum-events to listen to event

--- management/web3/multicall_custome.py
+++ management/web3/multicall_custome.py
@@ -1,36 +1,16 @@
-import asyncio
-from time import time
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import List, Optional
 
-import aiohttp
-import requests
 from web3 import Web3
 
-from multicall import Call
-from multicall.constants import (
+from utils.multicall import Call
+from utils.multicall.constants import (
     GAS_LIMIT,
-    MULTICALL2_ADDRESSES,
-    MULTICALL2_BYTECODE,
-    MULTICALL_ADDRESSES,
     w3,
 )
-from multicall.loggers import setup_logger
-from multicall.utils import (
-    await_awaitable,
+from utils.multicall.utils import (
     chain_id,
-    gather,
-    run_in_subprocess,
-    state_override_supported,
 )
-from multicall.multicall import (
-    NotSoBrightBatcher,
-    _raise_or_proceed,
-    batcher,
-    unpack_batch_results,
-    get_args,
-    unpack_aggregate_outputs,
-    CallResponse,
-    logger,
+from utils.multicall.multicall import (
     Multicall,
 )
 
@@ -51,16 +31,6 @@ class MulticallCustom(Multicall):
         self.w3 = _w3
         self.chainid = chain_id(self.w3)
         if require_success is True:
-            multicall_map = (
-                MULTICALL_ADDRESSES
-                if self.chainid in MULTICALL_ADDRESSES
-                else MULTICALL2_ADDRESSES
-            )
-            self.multicall_sig = "aggregate((address,bytes)[])(uint256,bytes[])"
-        else:
-            multicall_map = MULTICALL2_ADDRESSES
             self.multicall_sig = "tryBlockAndAggregate(bool,(address,bytes)[])(uint256,uint256,(bool,bytes)[])"
         # self.multicall_address = multicall_map[self.chainid]
         self.multicall_address = "0xca11bde05977b3631167028862be2a173976ca11"
-
-    
---
+++ utils/multicall/__init__.py
@@ -0,0 +1,3 @@
+from utils.multicall.signature import Signature
+from utils.multicall.call import Call
+from utils.multicall.multicall import Multicall
