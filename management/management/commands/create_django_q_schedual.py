4b11ab9778cf526921193e784262d2ced5c2f44b
LJlkdskdjflsa <liliangjya@gmail.com>
2022-10-19 13:15:58 +0800

    renew create_django_q_schedual command

--- management/management/commands/create_django_q_schedual.py
+++ management/management/commands/create_django_q_schedual.py
@@ -7,6 +7,7 @@ class Command(BaseCommand):
     """Use only for development!"""
 
     def handle(self, *args, **options):
+        # update fund price
         Schedule.objects.create(
             func="fund.tasks.update_funds_price",
             name="Update Fund Price",
@@ -14,12 +15,23 @@ class Command(BaseCommand):
             schedule_type=Schedule.HOURLY,
         )
 
+        # update mock oracle price
         Schedule.objects.create(
             func="management.tasks.multicall_price_feed.manage_price_feed",
             name="Management Price Feed",
             repeats=-1,
             schedule_type=Schedule.HOURLY,
         )
+
+        # pangolin liquidity management
+        Schedule.objects.create(
+            func="management.tasks.multicall_price_feed.manage_price_feed",
+            name="Pangolin Liquidity Management",
+            repeats=-1,
+            schedule_type=Schedule.HOURLY,
+        )
+
+        # listing events
         Schedule.objects.create(
             func="django_ethereum_events.tasks.event_listener",
             name="Listening Events",
